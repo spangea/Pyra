166d26a7f5c820bef54dca5baccffee874f5644d
Greta Dolcetti <53076361+gretadolcetti@users.noreply.github.com>
2024-09-10 11:33:51 +0200

    Statistical Warning

---
+++ src/lyra/core/statistical_warnings.py
@@ -0,0 +1,26 @@
+class TypeChangedWarning(UserWarning):
+    pass
+
+class InconsistentTypeWarning(UserWarning):
+    pass
+
+class GmeanWarning(UserWarning):
+    pass
+
+class SpecialSubscriptionAssignmentWarning(UserWarning):
+    pass
+
+class NoneRetAssignmentWarning(UserWarning):
+    pass
+
+class CategoricalConversionMean(UserWarning):
+    pass
+
+class ScaledMean(UserWarning):
+    pass
+
+class SpecialAccessFieldWarning(UserWarning):
+    pass
+
+class CategoricalPlot(UserWarning):
+    pass
