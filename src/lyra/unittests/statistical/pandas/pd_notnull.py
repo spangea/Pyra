67b327867b33ec33c4ec6a32130d47aa6be41849
Greta Dolcetti <53076361+gretadolcetti@users.noreply.github.com>
2024-09-24 17:07:17 +0200

    Improved semantics for notnull

--- src/lyra/unittests/statistical/pandas/pd_notnull.py
+++ src/lyra/unittests/statistical/pandas/pd_notnull.py
@@ -1,18 +1,23 @@
 import pandas as pd
-# STATE: df -> Top; df2 -> Top; s1 -> Top; s2 -> Top; s3 -> Top; z1 -> Top; z2 -> Top; z3 -> Top
+import numpy as np
+# STATE: array -> Top; df -> Top; df2 -> Top; s1 -> Top; s2 -> Top; s3 -> Top; z1 -> Top; z2 -> Top; z3 -> Top; z4 -> Top
 df = pd.read_csv("...")
-# STATE: df -> DataFrame; df2 -> Top; s1 -> Top; s2 -> Top; s3 -> Top; z1 -> Top; z2 -> Top; z3 -> Top
+# STATE: array -> Top; df -> DataFrame; df2 -> Top; s1 -> Top; s2 -> Top; s3 -> Top; z1 -> Top; z2 -> Top; z3 -> Top; z4 -> Top
 df2 = df.notnull()
-# STATE: df -> DataFrame; df2 -> DataFrame; s1 -> Top; s2 -> Top; s3 -> Top; z1 -> Top; z2 -> Top; z3 -> Top
+# STATE: array -> Top; df -> DataFrame; df2 -> DataFrame; s1 -> Top; s2 -> Top; s3 -> Top; z1 -> Top; z2 -> Top; z3 -> Top; z4 -> Top
 s1 = df["b"]
-# STATE: df -> DataFrame; df2 -> DataFrame; s1 -> Series; s2 -> Top; s3 -> Top; z1 -> Top; z2 -> Top; z3 -> Top
+# STATE: array -> Top; df -> DataFrame; df2 -> DataFrame; s1 -> Series; s2 -> Top; s3 -> Top; z1 -> Top; z2 -> Top; z3 -> Top; z4 -> Top
 s2 = df["a"].notnull()
-# STATE: df -> DataFrame; df2 -> DataFrame; s1 -> Series; s2 -> Series; s3 -> Top; z1 -> Top; z2 -> Top; z3 -> Top
+# STATE: array -> Top; df -> DataFrame; df2 -> DataFrame; s1 -> Series; s2 -> Series; s3 -> Top; z1 -> Top; z2 -> Top; z3 -> Top; z4 -> Top
 s3 = s1.notnull()
-# STATE: df -> DataFrame; df2 -> DataFrame; s1 -> Series; s2 -> Series; s3 -> Series; z1 -> Top; z2 -> Top; z3 -> Top
+# STATE: array -> Top; df -> DataFrame; df2 -> DataFrame; s1 -> Series; s2 -> Series; s3 -> Series; z1 -> Top; z2 -> Top; z3 -> Top; z4 -> Top
 z1 = pd.notnull(df)
-# STATE: df -> DataFrame; df2 -> DataFrame; s1 -> Series; s2 -> Series; s3 -> Series; z1 -> DataFrame; z2 -> Top; z3 -> Top
+# STATE: array -> Top; df -> DataFrame; df2 -> DataFrame; s1 -> Series; s2 -> Series; s3 -> Series; z1 -> DataFrame; z2 -> Top; z3 -> Top; z4 -> Top
 z2 = pd.notnull(s3)
-# STATE: df -> DataFrame; df2 -> DataFrame; s1 -> Series; s2 -> Series; s3 -> Series; z1 -> DataFrame; z2 -> Series; z3 -> Top
-z3 =  pd.notnull("")
-# FINAL: df -> DataFrame; df2 -> DataFrame; s1 -> Series; s2 -> Series; s3 -> Series; z1 -> DataFrame; z2 -> Series; z3 -> Boolean
+# STATE: array -> Top; df -> DataFrame; df2 -> DataFrame; s1 -> Series; s2 -> Series; s3 -> Series; z1 -> DataFrame; z2 -> Series; z3 -> Top; z4 -> Top
+z3 = pd.notnull("")
+# STATE: array -> Top; df -> DataFrame; df2 -> DataFrame; s1 -> Series; s2 -> Series; s3 -> Series; z1 -> DataFrame; z2 -> Series; z3 -> Boolean; z4 -> Top
+array = np.array([1, 2, 3, 4, 5, 6])
+# STATE: array -> NumericArray; df -> DataFrame; df2 -> DataFrame; s1 -> Series; s2 -> Series; s3 -> Series; z1 -> DataFrame; z2 -> Series; z3 -> Boolean; z4 -> Top
+z4 = pd.notnull(array)
+# FINAL: array -> NumericArray; df -> DataFrame; df2 -> DataFrame; s1 -> Series; s2 -> Series; s3 -> Series; z1 -> DataFrame; z2 -> Series; z3 -> Boolean; z4 -> BoolArray
