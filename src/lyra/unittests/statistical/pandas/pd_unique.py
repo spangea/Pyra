87cba500c55874685de0ce234da712f91e0e9706
VincenzoArceri <vincenzoarceri.92@gmail.com>
2024-10-04 15:53:07 +0200

    #11 #18 improved test for pandas unique method

--- src/lyra/unittests/statistical/pandas/pd_unique.py
+++ src/lyra/unittests/statistical/pandas/pd_unique.py
@@ -1,6 +1,15 @@
 import pandas as pd
-# STATE: df -> Top; x -> Top
+# STATE: a -> Top; b -> Top; c -> Top; d -> Top; df -> Top; e -> Top
 df = pd.DataFrame()
-# STATE: df -> DataFrame; x -> Top
-x = df['rate'].unique()
-# FINAL: df -> DataFrame; x -> Array
+# STATE: a -> Top; b -> Top; c -> Top; d -> Top; df -> DataFrame; e -> Top
+a = df['rate'].unique()
+# STATE: a -> Array; b -> Top; c -> Top; d -> Top; df -> DataFrame; e -> Top
+b = df['a'] / df['b']
+# STATE: a -> Array; b -> RatioSeries; c -> Top; d -> Top; df -> DataFrame; e -> Top
+c = b.unique()
+# STATE: a -> Array; b -> RatioSeries; c -> NumericArray; d -> Top; df -> DataFrame; e -> Top
+d = df.duplicated()
+# STATE: a -> Array; b -> RatioSeries; c -> NumericArray; d -> BoolSeries; df -> DataFrame; e -> Top
+e = d.unique()
+# FINAL: a -> Array; b -> RatioSeries; c -> NumericArray; d -> BoolSeries; df -> DataFrame; e -> BoolArray
+
