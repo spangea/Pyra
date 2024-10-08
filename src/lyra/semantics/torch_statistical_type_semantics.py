1f27ed72b2ff4d7969e612a01fce93403295b924
Greta Dolcetti <53076361+gretadolcetti@users.noreply.github.com>
2024-10-08 14:45:48 +0200

    Closes #117

--- src/lyra/semantics/torch_statistical_type_semantics.py
+++ src/lyra/semantics/torch_statistical_type_semantics.py
@@ -1,3 +1,31 @@
+from lyra.core.expressions import (
+    Subscription,
+    Input,
+)
+from lyra.core.statements import (
+    Call,
+    SubscriptionAccess,
+    Keyword,
+    LiteralEvaluation,
+    ListDisplayAccess,
+    TupleDisplayAccess,
+)
+from lyra.core.types import (
+    TopLyraType,
+)
+from lyra.engine.forward import ForwardInterpreter
+
+
+from lyra.statistical.statistical_type_domain import (
+    StatisticalTypeState,
+    StatisticalTypeLattice,
+)
+
+import lyra.semantics.utilities as utilities
 
 class TorchStatisticalTypeSemantics:
-    pass
+    def empty_cache_call_semantics(
+        self, stmt: Call, state: StatisticalTypeState, interpreter: ForwardInterpreter
+    ) -> StatisticalTypeState:
+        state.result = {StatisticalTypeLattice.Status.NoneRet}
+        return state
