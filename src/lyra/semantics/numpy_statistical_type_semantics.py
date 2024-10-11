09be188cb4611bb44114b76206a93a0b22277d17
VincenzoArceri <vincenzoarceri.92@gmail.com>
2024-10-11 13:06:54 +0200

    Added semantics file for numpy, numpy concatenate

---
+++ src/lyra/semantics/numpy_statistical_type_semantics.py
@@ -0,0 +1,18 @@
+from lyra.core.statements import Call
+
+from lyra.engine.forward import ForwardInterpreter
+
+from lyra.statistical.statistical_type_domain import (
+    StatisticalTypeState,
+    StatisticalTypeLattice
+)
+
+import lyra.semantics.utilities as utilities
+
+class NumPyStatisticalTypeSemantics:
+
+    def concatenate_call_semantics(
+            self, stmt: Call, state: StatisticalTypeState, interpreter: ForwardInterpreter
+    ) -> StatisticalTypeState:
+        state.result = {StatisticalTypeLattice.Status.Array}
+        return state
