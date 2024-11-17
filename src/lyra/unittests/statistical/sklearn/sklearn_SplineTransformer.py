9da96736424787ca73233c7f9639c537573de4b6
Francesco Bernini <francesco.bernini.2k@gmail.com>
2024-11-17 17:49:54 +0100

    Partial addition of Scalers tests and semantics

---
+++ src/lyra/unittests/statistical/sklearn/sklearn_SplineTransformer.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from sklearn.preprocessing import SplineTransformer
+# STATE: deanormalized_mean -> Top; df -> Top; scaler -> Top; wrong_normalized_mean -> Top; x -> Top
+x = [1, 2, 3, 4, 5]
+# STATE: deanormalized_mean -> Top; df -> Top; scaler -> Top; wrong_normalized_mean -> Top; x -> NumericList
+df = pd.DataFrame(x, columns=['x'])
+# STATE: deanormalized_mean -> Top; df -> DataFrame; scaler -> Top; wrong_normalized_mean -> Top; x -> NumericList
+scaler = SplineTransformer()
+# STATE: deanormalized_mean -> Top; df -> DataFrame; scaler -> SplineTransformer; wrong_normalized_mean -> Top; x -> NumericList
+scaler.fit(df[['x']])
+# STATE: deanormalized_mean -> Top; df -> DataFrame; scaler -> SplineTransformer; wrong_normalized_mean -> Top; x -> NumericList
+df['x_normalized'] = scaler.transform(df[['x']])
+# STATE: deanormalized_mean -> Top; df -> DataFrame; df["x_normalized"] -> NormSeries; scaler -> SplineTransformer; wrong_normalized_mean -> Top; x -> NumericList
+wrong_normalized_mean = df['x_normalized'].mean()
+# STATE: deanormalized_mean -> Top; df -> DataFrame; df["x_normalized"] -> NormSeries; scaler -> SplineTransformer; wrong_normalized_mean -> Numeric; x -> NumericList
+deanormalized_mean = scaler.inverse_transform(df[['x_normalized']]).mean()
+# FINAL: deanormalized_mean -> Numeric; df -> DataFrame; df["x_normalized"] -> NormSeries; scaler -> SplineTransformer; wrong_normalized_mean -> Numeric; x -> NumericList
+
+# FIXME: check if transform and fit can be called like this
